e76575772f6cbdafb89bee2db4be1efdb4981d43
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2018-10-15 20:40:37 +0200

    Changes in publish/subscribe pattern

--- Emitter/Program.cs
+++ Emitter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using RabbitMQ.Client;
 using System.Text;
+using System.Linq;
 
 namespace Emitter
 {
@@ -13,18 +14,50 @@ namespace Emitter
             {
                 using(var channel = connection.CreateModel())
                 {
-                    channel.ExchangeDeclare(exchange: "logs", type: "fanout");
-                    var message = GetMessage(args);
+                    
+                    // channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                    // Change from fanout to direct type
+                    // channel.ExchangeDeclare(exchange: "direct_logs", type: "direct");
+
+                    // Topic
+                    channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+                    var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
+
+                    var message = GetMessage(args.Skip(1).ToArray());
                     var body = Encoding.UTF8.GetBytes(message);
 
+                    // Exchange
+                    // channel.BasicPublish(
+                    //     exchange: "logs",
+                    //     routingKey: "",
+                    //     basicProperties: null,
+                    //     body: body
+                    // );
+
+                    // Direct
+                    // var serverities = new[] {"critical", "log", "warn"};
+                    // foreach(var serverity in serverities)
+                    // {
+                    //     channel.BasicPublish(
+                    //         exchange: "direct_logs",
+                    //         routingKey: serverity,
+                    //         basicProperties: null,
+                    //         body: body
+                    //     );
+                    // }
+
+                    // Topic
+
                     channel.BasicPublish(
-                        exchange: "logs",
-                        routingKey: "",
+                        exchange: "topic_logs",
+                        routingKey: routingKey,
                         basicProperties: null,
                         body: body
                     );
 
-                    Console.WriteLine($" [x] Sent {message}");
+                    //Console.WriteLine($" [x] Sent {message}");
+                    Console.WriteLine($" [x] Sent {routingKey}:{message}");
                 }
             }
         }
--- Receiver/Program.cs
+++ Receiver/Program.cs
@@ -17,20 +17,52 @@ namespace Receiver
                     channel.ExchangeDeclare(exchange: "logs", type: "fanout");
 
                     var queueName = channel.QueueDeclare().QueueName;
-                    channel.QueueBind(
-                        queue: queueName,
-                        exchange: "logs",
-                        routingKey: ""
-                    );
 
-                    Console.WriteLine($"Waiting for logs in queue: {queueName}");
+                    // topic
+                    if(args.Length < 1)
+                    {
+                        Console.Error.WriteLine("Usage: {0} [binding_key...]", Environment.GetCommandLineArgs()[0]);
+                            Console.WriteLine(" Press [enter] to exit.");
+                            Environment.ExitCode = 1;
+                            return;
+                    }
+                    
+                    // fanout
+                    // channel.QueueBind(
+                    //     queue: queueName,
+                    //     exchange: "logs",
+                    //     routingKey: ""
+                    // );
+
+                    // direct
+                    // foreach(var severity in args)
+                    // {
+                    //     channel.QueueBind(
+                    //         queue: queueName,
+                    //         exchange: "direct_logs",
+                    //         routingKey: severity
+                    //     );
+                    // }
+
+                    // Console.WriteLine($"Waiting for logs in queue: {queueName}");
+
+                    foreach(var bindingKey in args)
+                    {
+                        channel.QueueBind(queue: queueName,
+                                  exchange: "topic_logs",
+                                  routingKey: bindingKey);
+                    }
+
+                    Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
 
                     var consumer = new EventingBasicConsumer(channel);
                     consumer.Received += (model, ea) => 
                     {
                         var body = ea.Body;
                         var message = Encoding.UTF8.GetString(body);
-                        Console.WriteLine($" [x] {message}");
+                        var routingKey = ea.RoutingKey;
+                        //Console.WriteLine($" [x] {message}");
+                        Console.WriteLine($" [x] {routingKey}:{message}");
                     };
 
                     channel.BasicConsume(
